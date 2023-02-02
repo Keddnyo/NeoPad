416dd4e8c291f8da424511c277d27dbf00502830
Azat Mironov <65981689+Keddnyo@users.noreply.github.com>
2023-02-02 21:54:32 +0300

    App bar updated

---
+++ app/src/main/java/io/github/keddnyo/neopad/common/Constants.kt
@@ -0,0 +1,5 @@
+package io.github.keddnyo.neopad.common
+
+object Constants {
+    const val GITHUB_REPOSITORY = "https://github.com/Keddnyo/NeoPad"
+}
--- app/src/main/java/io/github/keddnyo/neopad/presentation/composable/main_screen/NoteListScreen.kt
+++ app/src/main/java/io/github/keddnyo/neopad/presentation/composable/main_screen/NoteListScreen.kt
@@ -1,6 +1,9 @@
 package io.github.keddnyo.neopad.presentation.composable.main_screen
 
+import android.app.Activity
 import android.app.Application
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -8,15 +11,14 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Create
+import androidx.compose.material.icons.twotone.Info
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +27,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import io.github.keddnyo.neopad.R
+import io.github.keddnyo.neopad.common.Constants
 import io.github.keddnyo.neopad.domain.model.Note
 import io.github.keddnyo.neopad.domain.navigation.NoteNavRoute
 import io.github.keddnyo.neopad.domain.viewmodel.MainViewModel
@@ -35,6 +38,8 @@ import io.github.keddnyo.neopad.presentation.ui.theme.NeoPadTheme
 @Composable
 fun NoteListScreen(navController: NavHostController, viewModel: MainViewModel) {
 
+    val context = LocalContext.current as Activity
+
     val notes = viewModel.getAllNotes().observeAsState(initial = listOf()).value
 
     NeoPadTheme {
@@ -44,9 +49,19 @@ fun NoteListScreen(navController: NavHostController, viewModel: MainViewModel) {
             Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
                 CenterAlignedTopAppBar(title = {
                     Text(
-                        text = stringResource(id = R.string.app_name),
-                        style = TextStyle(fontSize = 32.sp, fontFamily = FontFamily.Cursive)
+                        text = stringResource(id = R.string.app_name)
                     )
+                }, actions = {
+                    IconButton(onClick = {
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = Uri.parse(Constants.GITHUB_REPOSITORY)
+                        context.startActivity(intent)
+                    }) {
+                        Icon(
+                            imageVector = Icons.TwoTone.Info,
+                            contentDescription = stringResource(R.string.github)
+                        )
+                    }
                 })
             }, floatingActionButton = {
                 FloatingActionButton(onClick = {
@@ -83,7 +98,7 @@ fun NoteListScreen(navController: NavHostController, viewModel: MainViewModel) {
                             }
                             item {
                                 Spacer(
-                                    modifier = Modifier.height(32.dp)
+                                    modifier = Modifier.height(128.dp)
                                 )
                             }
                         }
